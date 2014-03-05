586a16c6d3fbf406c96f4b8b9b4aa44882745b91
Joshua Tauberer <jt@occams.info>
2014-03-05 08:09:56 -0500

    do a startup check that the Code repository is present

--- editor/index.js
+++ editor/index.js
@@ -1,25 +1,37 @@
 // Main entry point for the application server.
 
-var port = 8000;
+check_can_start();
+start_server();
 
-var express = require('express');
+function start_server() {
+	var port = 8000;
 
-var app = express();
+	var express = require('express');
 
-// general configuration
-app.set('title', 'Legal Code Editor');
-app.use(express.bodyParser());
-app.use("/static", express.static('static'))
+	var app = express();
 
+	// general configuration
+	app.set('title', 'Legal Code Editor');
+	app.use(express.bodyParser());
+	app.use("/static", express.static('static'))
 
-// configure routes
-var views = require('./views.js');
-views.set_routes(app);
+	// configure routes
+	var views = require('./views.js');
+	views.set_routes(app);
 
-// error conditions
-app.use(function(req, res, next){
-  res.send(404, 'Invalid URL.');
-});
+	// error conditions
+	app.use(function(req, res, next){
+	  res.send(404, 'Invalid URL.');
+	});
 
-console.log("listening on port", port);
-app.listen(port);
+	console.log("listening on port", port);
+	app.listen(port);
+}
+
+function check_can_start() {
+	// check that things are in place
+	var settings = require("./settings.js");
+	var path = require("path");
+	var fs = require("fs");
+	if (!fs.existsSync(settings.code_directory)) throw "Clone the code repository in " + path.normalize(settings.code_directory);
+}
