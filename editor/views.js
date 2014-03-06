bd676d235aec1703ca274baf189f7570bf0473eb
Joshua Tauberer <jt@occams.info>
2014-03-06 18:53:26 -0500

    When we render a page that contains a table of contents, the contents of referenced files may have changed also and that will change how the referenced file appears in the table of contents. So we must pass not only a particular section to render but any blob content of modified referenced pages.
    Now that tables of contents do render, rewrite the links to those pages
    to open up the correponding XML file in the editor.

--- editor/views.js
+++ editor/views.js
@@ -3,6 +3,8 @@ var pathlib  = require('path');
 var async = require('async');
 var patches = require("./patches.js");
 
+var settings = require("./settings.js");
+
 String.prototype.repeat = function( num ) { return new Array( num + 1 ).join( this ); }
 
 exports.set_routes = function(app) {
@@ -125,6 +127,7 @@ exports.set_routes = function(app) {
 			res.send(edit_file_template({
 				patch: patch,
 				filename: filename,
+				dirname: pathlib.dirname(filename),
 				base_text: JSON.stringify(base_text),
 				current_text: current_text
 			}));
@@ -149,35 +152,80 @@ exports.set_routes = function(app) {
 
 	// Render a Section
 	app.post('/render-body', function(req, res){
-		// Use simple-2 to render the section.
+		// Use simple-2 to render a preview of the page.
 
+		// We're passed the current contents of the file. Parse the XML.
 		var et = require('elementtree');
-		var render_body = require('../ext/simple-2/render_body.js');
 
 		var dom;
 		try {
 		 dom = et.parse(req.body.text)._root;
 		} catch (e) {
-			res.writeHead(400, {'Content-Type': 'application/json'});
+			res.writeHead(200, {'Content-Type': 'application/json'});
 			res.write(JSON.stringify( { "error": "Invalid XML: " + e } ));
 			res.end();
 			return;
 		}
 
-		var body;
-		try {
-			body = render_body.render_body("file.html", dom, null, null, null);
-		} catch (e) {
-			res.writeHead(400, {'Content-Type': 'application/json'});
-			res.write(JSON.stringify( { "error": "Something is wrong in the code: " + e } ));
-			res.end();
-			return;
-		}
+		// Because of XIncludes that may be present in the file, we may need the
+		// contents of other files in order to completely render this page. Get
+		// a list of all of the XInclude'd resources.
+		var other_resources_hrefs = Array();
+		dom.findall('.//ns0:include').forEach(function (elem) {
+			other_resources_hrefs.push(elem.get("href"));
+		});
 
-		res.setHeader('Content-Type', 'application/json');
-		res.send(JSON.stringify({
-			"status": "ok",
-			"html": body.rendered
-		}));
+		// Asynchronously load all of the binary contents of these resources.
+		var patch = patches.Patch.load(req.body.patch);
+		async.map(
+			other_resources_hrefs,
+			function(item, callback) {
+				var fn = pathlib.join(pathlib.dirname(req.body.path), item);
+				patch.getPathContent(fn, false, function(base_content, patch_content) {
+					try {
+						var other_dom = et.parse(patch_content)._root;
+						callback(null, [fn, other_dom]);
+					} catch (e) {
+						callback(e);
+					}
+				})
+			},
+			function(err, results) {
+				if (err) {
+					res.writeHead(200, {'Content-Type': 'application/json'});
+					res.write(JSON.stringify( { "error": "Invalid XML in included file " + elem.get("href") + ": " + e } ));
+					res.end();
+					return;
+				}
+
+				// turn the list of pairs of (filename, dom) into an Object.
+				var other_resources = {};
+				results.forEach(function(item) {
+					other_resources[item[0]] = item[1];
+				});
+
+				// finish the preview and send the response
+				finish_preview(req.body.path, dom, other_resources, res);
+			});
+
 	});
+}
+
+function finish_preview(fn, dom, other_resources, res) {
+	var render_body = require('../ext/simple-2/render_body.js');
+	var body;
+	try {
+		body = render_body.render_body(fn, dom, null, null, null, null, other_resources);
+	} catch (e) {
+		res.writeHead(200, {'Content-Type': 'application/json'});
+		res.write(JSON.stringify( { "error": "Something is wrong in the code: " + e } ));
+		res.end();
+		return;
+	}
+
+	res.setHeader('Content-Type', 'application/json');
+	res.send(JSON.stringify({
+		"status": "ok",
+		"html": body.rendered
+	}));
 }
