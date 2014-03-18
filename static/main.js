1aa1343b053738f46c20cf168f98702b8179db25
Joshua Tauberer <jt@occams.info>
2014-03-17 21:36:56 -0400

    update the confirmation modal to execute callbacks only after the modal is fully hidden so that future modals do not open while this one is closing, and allow for a cancellation callback, and also rename the OK button to a verb passed by the caller

--- static/main.js
+++ static/main.js
@@ -1,10 +1,22 @@
-var global_modal_ok_func = null;
+var global_modal_state = null;
+var global_modal_funcs = null;
 
 $(function() {
 	$('#global_modal .btn-danger').click(function() {
-		if (global_modal_ok_func)
-			global_modal_ok_func();
+		// Don't take action now. Wait for the modal to be totally hidden
+		// so that we don't attempt to show another modal while this one
+		// is closing.
+		global_modal_state = 0; // OK
 	})
+	$('#global_modal .btn-default').click(function() {
+		global_modal_state = 1; // Cancel
+	})
+	$('#global_modal').on('hidden.bs.modal', function (e) {
+		// do the cancel function
+		if (global_modal_state == null) global_modal_state = 1; // cancel if the user hit ESC or clicked outside of the modal
+		if (global_modal_funcs && global_modal_funcs[global_modal_state])
+			global_modal_funcs[global_modal_state]();
+	})	
 })
 
 function show_modal_error(title, message) {
@@ -12,16 +24,18 @@ function show_modal_error(title, message) {
 	$('#global_modal p').text(message);
 	$('#global_modal .btn-default').show().text("OK");
 	$('#global_modal .btn-danger').hide();
-	global_modal_ok_func = null;
+	global_modal_funcs = null;
+	global_modal_state = null;
 	$('#global_modal').modal({});
 }
 
-function show_modal_yes_no_question(title, question, yes_callback) {
+function show_modal_confirm(title, question, verb, yes_callback, cancel_callback) {
 	$('#global_modal h4').text(title);
 	$('#global_modal p').text(question);
-	$('#global_modal .btn-default').show().text("No");
-	$('#global_modal .btn-danger').show().text("Yes");
-	global_modal_ok_func = yes_callback;
+	$('#global_modal .btn-default').show().text("Cancel");
+	$('#global_modal .btn-danger').show().text(verb);
+	global_modal_funcs = [yes_callback, cancel_callback];
+	global_modal_state = null;
 	$('#global_modal').modal({});
 }
 
