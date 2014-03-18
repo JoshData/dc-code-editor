0050ba82cb65b97f15ef0c16a17bdae2e96ccf71
Joshua Tauberer <jt@occams.info>
2014-03-17 21:34:21 -0400

    show a working indicator while ajax calls are running

--- static/main.js
+++ static/main.js
@@ -25,20 +25,37 @@ function show_modal_yes_no_question(title, question, yes_callback) {
 	$('#global_modal').modal({});
 }
 
-function ajax_call(url, data, success_callback, modal_error_title) {
+var is_ajax_loading = false;
+function ajax_call(url, data, success_callback, modal_error_title, failure_callback) {
+	// Show a modal progress indicator that prevents other page interactions
+	// while the call is processing. Delay before showing the indicator so
+	// that we don't flash something only just for a moment.
+	is_ajax_loading = true;
+	setTimeout("if (is_ajax_loading) $('#ajax_loading_indicator').fadeIn()", 100);
+	function hide_loading_indicator() {
+		is_ajax_loading = false;
+		$('#ajax_loading_indicator').hide();
+	}
+
 	$.ajax(
 		url,
 		{
 			data: data,
 			method: "POST",
 			success: function(res) {
-				if (res.status == "error")
+				hide_loading_indicator();
+				if (res.status == "error") {
 					show_modal_error(modal_error_title, res.msg);
-				else
+					if (failure_callback) failure_callback();
+				}
+				else {
 					success_callback(res);
+				}
 			},
 			error: function() {
+				hide_loading_indicator();
 				show_modal_error(modal_error_title, "Sorry, an internal error occurred.");
+				if (failure_callback) failure_callback();
 			}
 	});
 }
